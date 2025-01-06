7d412f98cf915a8f7cc03d2fb4a566a5dd646c5d
Szymon <szekej51@gmail.com>
2025-01-06 13:46:29 +0100

    router module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()), 
     provideAnimationsAsync(),
     importProvidersFrom(ReactiveFormsModule),
-    importProvidersFrom(RouterModule),
+    importProvidersFrom(RouterModule), provideAnimationsAsync(),
   ]
 };
