ee6f6ccfd25dee374fc7c8d151090255f3463774
Szymon <szekej51@gmail.com>
2025-01-06 13:41:29 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,41 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ChildComponent } from './components/child/child.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true, 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent, FooterComponent,ChildComponent], 
+  imports: [HeaderComponent, FooterComponent, ChildComponent, CommonModule, ReactiveFormsModule, RouterModule], 
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class AppComponent {
   title = 'my-angular-app';
   messageFromChild = ''; 
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // inicjalizacja formularza
+    this.userForm = this.fb.group({
+      name: ['', Validators.required], 
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    } else {
+      console.log('Formularz zawiera błędy.');
+    }
+  }
 
   receiveMessage(message: string) {
     this.messageFromChild = message;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    importProvidersFrom(ReactiveFormsModule),
+    importProvidersFrom(RouterModule),
   ]
 };
